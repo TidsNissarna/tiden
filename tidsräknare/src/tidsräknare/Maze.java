098cb6b759a3539b9d740f4f8a48c392e379bbd3
Bobbimon <tobbe_lobbe88@hotmail.com>
2015-02-04 01:02:21 +0100

    Ett fönster!! :D

--- tidsräknare/src/tidsräknare/Maze.java
+++ tidsräknare/src/tidsräknare/Maze.java
@@ -1,10 +1,24 @@
 package tidsräknare;
 
+import javax.swing.JFrame;
+
 public class Maze {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new Maze();
 
+	}
+	
+	public Maze(){
+		JFrame f = new JFrame();
+		f.setTitle("Maze game");
+		//f.add(new board())
+		f.setSize(500,400);
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 
 }
